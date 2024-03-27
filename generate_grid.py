53d4448448fa99c7279c9fb0eb89f4b00e04e1ec
Patateon <maximebeldjilali@gmail.com>
2024-03-27 15:34:41 +0100

    generate grid mais propre

--- generate_grid.py
+++ generate_grid.py
@@ -1,20 +1,18 @@
 import itertools
 import os
 
-MAX_GRID = 1000000
+MAX_GRID = 10
 SIZE = 6
 
 def generate_all_possible_boards():
     all_boards = []
     # Générer toutes les combinaisons possibles de la grille
     empty_cells = " " * SIZE * SIZE
-    cnt = 0
     for combination in itertools.product("XO ", repeat=SIZE * SIZE):
-        cnt += 1
         board = "".join(combination)
         if board.count("X") == board.count("O") or board.count("X") == board.count("O") + 1:
             all_boards.append(board)
-            if (cnt > MAX_GRID):
+            if (len(all_boards) >= MAX_GRID):
                 return all_boards
     return all_boards
 
