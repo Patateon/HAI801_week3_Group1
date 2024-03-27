823d0f2deca6e00d07f967444e69ae7cb9cdebae
Patateon <maximebeldjilali@gmail.com>
2024-03-27 13:51:06 +0100

    update v2

---
+++ generate_grid.py
@@ -0,0 +1,26 @@
+import itertools
+import os
+
+MAX_GRID = 1000000
+SIZE = 5
+
+def generate_all_possible_boards():
+    all_boards = []
+    # Générer toutes les combinaisons possibles de la grille
+    empty_cells = " " * SIZE * SIZE
+    cnt = 0
+    for combination in itertools.product("XO ", repeat=SIZE * SIZE):
+        cnt += 1
+        board = "".join(combination)
+        if board.count("X") == board.count("O") or board.count("X") == board.count("O") + 1:
+            all_boards.append(board)
+            if (cnt > MAX_GRID):
+                return all_boards
+    return all_boards
+
+if __name__ == "__main__":
+    all_possible_boards = generate_all_possible_boards()
+
+    datapath = os.path.join("data", f"dataset-{SIZE}.txt")
+    with open(datapath, 'w') as f:
+        f.write("\n".join(all_possible_boards))
--- morpion.cpp
+++ morpion.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const int SIZE = 3;
+const int SIZE = 5;
 const char EMPTY = '-';
 const char COMPUTER = 'X';
 const char HUMAN = 'O';
@@ -37,40 +37,49 @@ void Morpion::jouer(const char* nomFichier) {
     while (getline(fichier, ligne)) {
         vector<char> grille;
         for (char c : ligne) {
-            if (c != ' ')
+            if (c != ' '){
                 grille.push_back(c);
+            }else{
+                grille.push_back(EMPTY);
+            }
         }
         if (grille.size() == SIZE * SIZE)
             grilles.push_back(grille);
+        else{
+            cout<<grille.size()<<", "<<SIZE*SIZE<<endl;
+        }
     }
 
     fichier.close();
 
-    for (auto& grille : grilles) {
+    cout<<"Start parsing..."<<endl;
+    for (int k = 0; k < grilles.size(); k++){
         for (int i = 0; i < SIZE; ++i) {
             for (int j = 0; j < SIZE; ++j) {
-                plateau[i][j] = grille[i * SIZE + j];
+                plateau[i][j] = grilles[k][i * SIZE + j];
             }
         }
 
-        afficherPlateau();
+    cout<<"Start solving..."<<endl;
 
-        int score = minimax(COMPUTER);
+    int score = minimax(COMPUTER);
 
-        if (score > 0){
-            cout << "L'ordinateur a gagné !" << endl;
-            nbDefaite +=1;
-        }
-        else if (score < 0){
-            cout << "Le joueur humain a gagné !" << endl;
-            nbVictoire ++;
-        }
-        else{
-            cout << "Match nul !" << endl;
-            nbNul++;
-        }
+    afficherPlateau();
 
-        cout << endl;
+    if (score > 0){
+        cout << "L'ordinateur a gagné !" << endl;
+        nbVictoire++;
+    }
+    else if (score < 0){
+        cout << "Le joueur humain a gagné !" << endl;
+        nbDefaite++;
+    }
+    else{
+        cout << "Match nul !" << endl;
+        nbNul++;
+    }
+
+    cout << endl;
     }
 }
 
@@ -120,14 +129,33 @@ bool Morpion::estPlein() {
 
 bool Morpion::estGagnant(char joueur) {
     for (int i = 0; i < SIZE; ++i) {
-        if (plateau[i][0] == joueur && plateau[i][1] == joueur && plateau[i][2] == joueur)
+        bool ligne = true;
+        for (int j = 0; j < SIZE; j++){
+            ligne = ligne && plateau[i][j] == joueur;
+        }
+        if (ligne)
             return true;
-        if (plateau[0][i] == joueur && plateau[1][i] == joueur && plateau[2][i] == joueur)
+
+        bool colonne = true;
+        for (int j = 0; j < SIZE; j++){
+            colonne = colonne && plateau[j][i] == joueur;
+        }
+        if (colonne)
             return true;
     }
-    if (plateau[0][0] == joueur && plateau[1][1] == joueur && plateau[2][2] == joueur)
+
+    bool diagonal = true;
+    for (int j = 0; j < SIZE; j++){
+        diagonal = diagonal && plateau[j][j] == joueur;
+    }
+    if (diagonal)
         return true;
-    if (plateau[0][2] == joueur && plateau[1][1] == joueur && plateau[2][0] == joueur)
+
+    diagonal = true;
+    for (int j = 0; j < SIZE; j++){
+        diagonal = diagonal && plateau[j][SIZE-1-j] == joueur;
+    }
+    if (diagonal)
         return true;
     return false;
 }
